2ed441ae92bb9a0b80b7864ba339e49eda8351b2
Jonas Wende <jonas.wende@tum.de>
2024-08-14 12:06:38 +0200

    Refine the ranking system

--- src/main/java/de/techwende/api/service/RankService.java
+++ src/main/java/de/techwende/api/service/RankService.java
@@ -6,39 +6,15 @@ import de.techwende.api.domain.RankingEntry;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class RankService {
+    private static final RankingEntry NO_ENTRY = new RankingEntry(Integer.MAX_VALUE, 0);
 
-    private static final double WEIGHT_FACTOR = 0.5;
-
-    public static Ranking rank(List<Ranking> preferences) {
-        Set<AgendaItem> items = preferences.stream().flatMap(r -> r.getItems().stream()).collect(Collectors.toSet());
-        Ranking rankingResult = new Ranking();
-
-        for (AgendaItem item : items) {
-            Set<RankingEntry> prefsSet =
-                    preferences.stream().map(r -> r.getRank(item)).filter(Optional::isPresent).map(Optional::get)
-                            .collect(Collectors.toSet());
-            double avgRank = prefsSet.stream().mapToDouble(RankingEntry::rank).average().orElse(0);
-            double avgWeight = prefsSet.stream().mapToDouble(RankingEntry::weight).average().orElse(0);
-            rankingResult.setItem(item, avgRank, avgWeight);
-        }
-
-        return rankingResult;
-    }
-
-    public static List<AgendaItem> resultListOrdered(Ranking result) {
-        return result.getItems().stream().sorted(
-                        Comparator.comparingDouble(
-                                r -> calculateItemScore(result.getRank(r).orElse(new RankingEntry(-1, -1)))))
+    private static List<AgendaItem> resultOrdered(Ranking result) {
+        return result.getItems().stream()
+                .sorted(Comparator.comparingInt(r -> result.getRank(r).orElse(NO_ENTRY).rank()))
                 .collect(Collectors.toList());
-    }
-
-    private static double calculateItemScore(RankingEntry entry) {
-        return entry.rank() / (WEIGHT_FACTOR * entry.weight());
     }
 
 }
