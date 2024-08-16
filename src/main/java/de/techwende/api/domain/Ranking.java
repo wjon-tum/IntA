37a3faecaff02ca350ea560712fc3468ae2500e2
Jonas Wende <jonas.wende@tum.de>
2024-08-16 22:02:21 +0200

    Add first draft of HemmingDistance ranking strategy

--- src/main/java/de/techwende/api/domain/Ranking.java
+++ src/main/java/de/techwende/api/domain/Ranking.java
@@ -1,7 +1,10 @@
 package de.techwende.api.domain;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 
@@ -23,6 +26,13 @@ public class Ranking {
 
     public void deleteItem(AgendaItem item) {
         rankings.remove(item);
+    }
+
+    public List<AgendaItem> getItemsOrdered() {
+        return rankings.entrySet().stream()
+                .sorted(Comparator.comparingInt(e -> e.getValue().rank()))
+                .map(Entry::getKey)
+                .toList();
     }
 
     public Set<AgendaItem> getItems() {
